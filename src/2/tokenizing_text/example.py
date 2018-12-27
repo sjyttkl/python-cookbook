fbf2978b54519b6756b5d12dca9a3e08efe04ec4
songdongdong2 <songdongdong@jd.com>
2018-12-27 17:39:33 +0800

    厉害了，带组名的 正则表达式

--- src/2/tokenizing_text/example.py
+++ src/2/tokenizing_text/example.py
@@ -4,7 +4,7 @@
 
 import re
 from collections import namedtuple
-
+# 下面的形式，带组名 (?P<name>…)，
 NAME = r'(?P<NAME>[a-zA-Z_][a-zA-Z_0-9]*)'
 NUM  = r'(?P<NUM>\d+)'
 PLUS = r'(?P<PLUS>\+)'
@@ -19,6 +19,7 @@ Token = namedtuple('Token', ['type','value'])
 def generate_tokens(pat, text):
     scanner = pat.scanner(text)
     for m in iter(scanner.match, None):
+        print(m.lastgroup,m.group())
         yield Token(m.lastgroup, m.group())
 
 for tok in generate_tokens(master_pat, 'foo = 42'):
